e4b529646780f7dae2e8341a7d4cc28d3e5df6dc
Jai Lalawat <jailalawat@gmail.com>
2026-03-18 20:43:18 +0530

    fix: add missing package declaration and Lottie import to resolve build errors

--- mobile/app/src/main/kotlin/com/jdasense/app/ResultActivity.kt
+++ mobile/app/src/main/kotlin/com/jdasense/app/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.airbnb.android.lottie.LottieAnimationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.jdasense.app.databinding.ActivityResultBinding
--- mobile/app/src/main/kotlin/com/jdasense/app/SplashActivity.kt
+++ mobile/app/src/main/kotlin/com/jdasense/app/SplashActivity.kt
@@ -1,3 +1,5 @@
+package com.jdasense.app
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
